ef293b8b9a298f84dcab4aa7f6f1ddd7f80447c0
fanil279 <fanilramakaev@gmail.com>
2026-02-25 15:35:51 +0500

    refactor: restructure ConfirmationDialog component to include useEffect for demonstration of memory leak

--- homeAssignments/stopwatch/src/pages/StopwatchPage/modals/ConfirmationModal.tsx
+++ homeAssignments/stopwatch/src/pages/StopwatchPage/modals/ConfirmationModal.tsx
@@ -1,31 +1,50 @@
-import type { FC } from 'react';
+import { type FC, useEffect } from 'react';
 import Button from '../../../components/Button';
 import type { ConfirmDialogProps } from '../../../types';
 
-const ConfirmationDialog: FC<ConfirmDialogProps> = ({ onCancel, onConfirm }) => (
-    <div className='confirmation-dialog-wrapper'>
-        <div className='confirmation-dialog'>
-            <h2 className='confirmation-dialog__title'>
-                Are you sure you want to add a stopwatch?
-            </h2>
+const ConfirmationDialog: FC<ConfirmDialogProps> = ({ onCancel, onConfirm }) => {
+    useEffect(() => {
+        const handleClick = (): void => {
+            console.log(
+                'Clicked detected. useEffect is not cleared. Memory Leak created.'
+            );
+        };
 
-            <div className='confirmation-dialog__buttons'>
-                <Button
-                    variant='danger'
-                    onClick={onCancel}
-                >
-                    Cancel
-                </Button>
-                
-                <Button
-                    variant='primary'
-                    onClick={onConfirm}
-                >
-                    Confirm
-                </Button>
+        document.documentElement.addEventListener('click', handleClick);
+
+        // In order to prevent memory leaks, we should clean up the event listener when the component unmounts as the code below does.
+        // However, in this example, we are intentionally not cleaning up the event listener to demonstrate a memory leak.
+
+        /* return () => {
+            document.documentElement.removeEventListener('click', handleClick);
+        }; */
+    }, []);
+
+    return (
+        <div className='confirmation-dialog-wrapper'>
+            <div className='confirmation-dialog'>
+                <h2 className='confirmation-dialog__title'>
+                    Are you sure you want to add a stopwatch?
+                </h2>
+
+                <div className='confirmation-dialog__buttons'>
+                    <Button
+                        variant='danger'
+                        onClick={onCancel}
+                    >
+                        Cancel
+                    </Button>
+                    
+                    <Button
+                        variant='primary'
+                        onClick={onConfirm}
+                    >
+                        Confirm
+                    </Button>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default ConfirmationDialog;
