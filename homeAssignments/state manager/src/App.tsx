d9457c1e9fbe716a519ddf9f1ae5c384ac10087e
fanil279 <fanilramakaev@gmail.com>
2026-04-07 15:16:22 +0500

    feat: implement components A and B with state management; add responsive styles

--- homeAssignments/state manager/src/App.tsx
+++ homeAssignments/state manager/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import A from './components/A';
+import B from './components/B';
 
 function App() {
 
 	return (
-		<>
-			
-		</>
+		<div className='components-container'>
+			<A />
+			<B />
+		</div>
 	);
 }
 
---
+++ homeAssignments/state manager/src/components/A.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from 'react';
+import { decrement, increment, store } from '../store/store';
+
+const A = () => {
+    const [count, setCount] = useState<number>(store.getState());
+    const [isSubscribed, setIsSubscribed] = useState<boolean>(true);
+    
+    const unsubscribeRef = useRef<null | (() => void)>(null);
+
+    useEffect(() => {
+        const unsubscribe = store.subscribe(() => {
+            setCount(store.getState());
+        });
+
+        unsubscribeRef.current = unsubscribe;
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    const handleUnsubscribe = () => {
+        if (unsubscribeRef.current) {
+            unsubscribeRef.current();
+            unsubscribeRef.current = null;
+            setIsSubscribed(false);
+        }
+    };
+
+    return (
+        <div>
+            <h2 className='component-a'>Component A</h2>
+            <p className='count'>Count: {count}</p>
+
+            <button className='increment' onClick={increment}>Increment</button>
+            <button className='decrement' onClick={decrement}>Decrement</button>
+
+            <button
+                className='unsubscribe'
+                onClick={handleUnsubscribe}
+                disabled={!isSubscribed}
+            >
+                Unsubscribe
+            </button>
+        </div>
+    );
+};
+
+export default A;
